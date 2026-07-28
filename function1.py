e40eec005ef01cd1a9b5cc5d745ca9e8527abc2e
sbhoomika069-png <sbhoomika069@gmail.com>
2026-07-28 19:16:17 +0530

    update

---
+++ function1.py
@@ -0,0 +1,79 @@
+'''# functions
+def calc_sum(a,b):# a and b parameters
+    sum = a + b
+    print(sum)
+    return sum
+
+calc_sum(2,3) # 2 and 3 are arguments
+calc_sum(7,8)
+calc_sum(9,10)
+
+# function defination
+def calc_sum(a,b):
+    return a + b
+
+sum = calc_sum(2,3) # function call; arguments
+print(sum)
+
+# print Hello
+def print_Hello():
+    print("Hello")
+    
+print_Hello()
+
+# average of three numbers
+def average(a, b, c):
+    sum = (a+b+c)/3
+    print(sum)
+    return sum
+
+average(3,4,5)
+average(40,50,60)
+
+# WAF to print the length of a list
+cities = ["delhi","gurgaon","noida","pune","mumbai","chennai"]
+heroes = ["thor", "ironman", "captain america", "shaktiman"]
+
+def print_len(list):
+    print(len(list))
+    
+print_len(cities)
+print_len(heroes)
+
+#WAF to print the elements of a list in a single line
+
+
+cities = ["delhi","gurgaon","noida","pune","mumbai","chennai"]
+heroes = ["thor", "ironman", "captain america", "shaktiman"]
+
+def print_list(list):
+    for item in list:
+        print(item , end = " ") #item end is used for nect like \n
+   
+print_list(heroes)
+print()
+print_list(cities)
+
+# Write to find the factorial of n.(n is the parameter)
+def cal_fact(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    print(fact)
+cal_fact(6)
+
+# WAP to convert USD to INR
+
+def converter(usd_val):
+    inr_val = usd_val * 83
+    print(usd_val, "USD=", inr_val, "INR")
+    
+converter(1)'''
+
+
+
+
+
+
+
+
