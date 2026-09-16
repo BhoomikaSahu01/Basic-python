7a2f508e134c0a5937a06d4bb4d381175b04c7f7
sbhoomika069-png <sbhoomika069@gmail.com>
2026-09-16 13:13:45 +0530

    update

---
+++ sets1.py
@@ -0,0 +1,24 @@
+# set
+'''s = {3, 23, 2, 11}
+print(s, type(s))
+#print(s[3]) #you are not allowed to do something like this
+
+
+s = {34, 23, 1, 3, 22}
+print(s)
+s.add(32)
+s.add(322)
+s.remove(23)
+#s.remove(45223)# it through error key error
+s.discard(45323) # don't through an error
+print(s)'''
+
+'''# set operation
+a = {3, 23, 1}
+b = {23, 4, 2, 55, 1}
+
+c = a.union(b)
+print(c)
+
+d = a.intersection(b)
+print(d)'''
---
+++ tuple1.py
@@ -0,0 +1,17 @@
+'''#Tuples
+a = (3, 2, 22, 13,)
+print(a)
+print(a[2])
+a[3] = 32
+b = (3, )#how do you create tuple with one element
+'''
+
+'''tuple Unpacking
+
+tu = (3, 2, 45)
+a, b, c = tu
+print(a, b, c) # 3, 2, 45
+
+t = (3, 12, 1, 54, 23, 12)
+print(t.count(12))
+print(t.index(12))'''
