0751565e0618d1d46a6f55cdd5c7f3445381966d
sbhoomika069-png <sbhoomika069@gmail.com>
2026-09-22 12:38:55 +0530

    update

--- Recursion1.py
+++ Recursion1.py
@@ -53,5 +53,131 @@ def show(n):
     print(n)
     show(n-1)
     
-show(5) # 5, 4= n-1, 3= n-2, 2= n-3, 1'''
+show(5) # 5, 4= n-1, 3= n-2, 2= n-3, 1
+
+def show(n):
+    if n==0 :
+        return
+    print(n)
+    show(n-1)
+    
+show(9)'''
+
+'''print numbers from 1 to N
+def print_num(n):
+    if n == 0:
+        return
+    print_num(n-1)
+    print(n)
+    
+print_num(9)'''
+
+'''print numbers from N to 1
+def print_num(n):
+    if n == 0:
+        return
+    print(n)
+    print_num(n-1)
+    
+print_num(8)'''
+
+'''print "Hello" N times
+def hello(n):
+    if n == 0:
+        return
+    print("hello")
+    hello(n - 1)
+    
+hello(9)'''
+
+'''Find Sum From 1 to N
+def total(n):
+    if n == 0:
+        return 0
+    return n + total(n-1)
+
+print(total(5))
+
+def total(n):
+    if n == 0:
+        return 0
+    return n + total(n - 1)
+print(total(5))'''
+
+'''find factorial
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(5))
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+print(factorial(5))'''
+
+'''print even numbers from 2 to N
+def even_numbers(n):
+    if n < 2:
+        return
+    
+    even_numbers(n - 1)
+    
+    if n % 2 == 0:
+        print(n)
+        
+even_numbers(6)
+
+def even_number(n):
+    if n < 2: # base case of recussion
+        return
+    even_number(n - 1) # recursive call
+    
+    if n % 2 == 0:
+        print(n)
+
+even_number(10)'''
+
+'''Print odd numbers from 1 to N
+def odd(n):
+    if n < 1:
+        return
+    odd(n -1)
+    if n % 2 != 0:
+        print(n)
+odd(10)'''
+
+'''Find a power of a number with recursion code
+def power(a, n):
+    if n == 0:
+        return 1
+    return a * power(a, n-1)
+print(power(2, 5))
+
+def power(a,n):
+    if n == 0:
+        return 1
+    return a * power(a, n-1)
+
+print(power(2, 5))
+
+def show(n):
+    if(n == 0):
+        return
+    print(n)
+    show(n - 1)
+    print("End")
+    
+show(3)'''
+     
+
+    
+    
+        
+    
+
+    
     
